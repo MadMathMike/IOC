6e63d1f4511233b41326f71aee7fc5e0c7885a23
Michael Peterson <MichaelPeterson27@live.com>
2017-07-12 00:42:59 -0600

    Added the ThreadStaticLifecycleManagerFactory (which allows someone to use a ThreadStaticLifecycleManager in the container), and added tests that worked with the factory and the container to ensure the correct behavior.

---
+++ Injected.Extended.Tests/ThreadStaticLifecycleManagerFactoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Injected.Extended.Tests.TestClasses;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Injected.Extended.Tests
+{
+    public class ThreadStaticLifecycleManagerFactoryTests
+    {
+        public class CreateLifecycleManager
+        {
+            [Fact]
+            public void CausesTheContainerToResolveToTheSameInstanceWhenCalledMultipletimes()
+            {
+                // arrange
+                var container = new DependencyInjectionContainer();
+                container.Register<A, A>(new ThreadStaticLifecycleManagerFactory<A>());
+
+                // act
+                var a1 = container.Resolve<A>();
+                var a2 = container.Resolve<A>();
+
+                // assert
+                Assert.Equal(a1, a2);
+            }
+
+            [Fact]
+            public async Task CausesTheContainerToResolveToDifferentInstancesWhenCalledFromDifferentThreads()
+            {
+                // arrange
+                var container = new DependencyInjectionContainer();
+                container.Register<A, A>(new ThreadStaticLifecycleManagerFactory<A>());
+
+                // act
+                var task1 = Task.Run(() => container.Resolve<A>());
+                var task2 = Task.Run(() => container.Resolve<A>());
+                var a1 = await task1;
+                var a2 = await task2;
+
+                // assert
+                Assert.NotEqual(a1, a2);
+            }
+        }
+    }
+}
---
+++ Injected.Extended/ThreadStaticLifecycleManagerFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Injected.Extended
+{
+    public class ThreadStaticLifecycleManagerFactory<T> : ILifecycleManagerFactory<T> where T : class
+    {
+        public ILifecycleManager<T> CreateLifecycleManager(Func<T> objectFactory)
+        {
+            return new ThreadStaticLifecycleManager<T>(objectFactory);
+        }
+    }
+}
